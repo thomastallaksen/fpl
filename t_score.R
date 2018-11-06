1679b7035f71cb120568f9e0cf3a50d13f83791f
Thomas Tallaksen <thomastallaksen@gmail.com>
2018-11-06 13:54:26 +0100

    Updated to create columns with mean_points_last_5 and difficulty_next_5. Also added a new score to choose captain, that only uses next rounds difficulty.

--- t_score.R
+++ t_score.R
@@ -3,28 +3,61 @@ library(tidyverse)
 library(openxlsx)
 
 
+players <- fpl_get_players()
+
+
+# Definerer funksjoner
+
 t_score <- function(x){
   player <- fpl_get_player_detailed(x)
   points_last_5 <- mean(tail(player$history$total_points, 5))
   difficulty_next_5 <- mean(head(player$fixtures$difficulty, 5))
-  t_score_result <- points_last_5/difficulty_next_5
-  return(t_score_result)
+  t_score <- points_last_5/difficulty_next_5
+  return(t_score)
 }
 
-players <- fpl_get_players()
+t_score_cpt <- function(x){
+  player <- fpl_get_player_detailed(x)
+  points_last_5 <- mean(tail(player$history$total_points, 5))
+  difficulty_next_5 <- head(player$fixtures$difficulty, 1)
+  t_score <- points_last_5/difficulty_next_5
+  return(t_score)
+}  
+
+points_last_5 <- function(x){
+  player <- fpl_get_player_detailed(x)
+  points_last_5 <- mean(tail(player$history$total_points, 5))
+  return(points_last_5)
+}
+
+difficulty_next_5 <- function(x){
+  player <- fpl_get_player_detailed(x)
+  difficulty_next_5 <- mean(head(player$fixtures$difficulty, 5))
+  return(difficulty_next_5)
+}
+
+
+# Anvender funksjonene per spiller
 
 players <- players%>%
   rowwise()%>%
+  mutate(difficulty_next_5 = difficulty_next_5(id))%>%
+  mutate(t_score_cpt = t_score_cpt(id))%>%
+  mutate(points_last_5 = points_last_5(id))%>%
   mutate(t_score = t_score(id))
 
-players <- players%>%
-  mutate(t_value = t_score/now_cost)
+
+# Velger ut relevante stats 
 
 players_utvalg <- players%>%
-  select(first_name, second_name, now_cost, position, t_score, t_value, chance_of_playing_this_round, selected_by_percent, form, total_points)%>%
+  select(first_name, second_name, team_name, now_cost, position, chance_of_playing_this_round, form, total_points, points_last_5, difficulty_next_5, t_score, t_score_cpt)%>%
   arrange(desc(t_score))
+
+
+# Printer til excel
   
 write.xlsx(players_utvalg, "t_score.xlsx")
+
 
 
 
